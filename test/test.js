e825f6fe00c154db8c46372041e52ab7b3041a8a
David Luecke <daff@neyeon.com>
2016-09-29 11:33:41 -0700

    Implementing file server functionality and tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,44 @@
 const assert = require('assert');
+const path = require('path');
+const request = require('request');
 const binstaface = require('../lib');
 
 describe('my app test', function() {
-  it('exports the message', function() {
-    assert.equal(binstaface.message, 'Hello world');
+  const PORT = 4455;
+  let server;
+
+  before(function(done) {
+    server = binstaface(path.join(__dirname, '..'))
+      .listen(PORT, () => done());
+  });
+
+  after(function(done) {
+    server.close(() => done());
+  });
+
+  it('sends 404 for non-existing files', function(done) {
+    request(`http://localhost:${PORT}/dummy.txt`, (error, res, body) => {
+      if(error) {
+        return done(error);
+      }
+
+      assert.equal(res.statusCode, 404);
+      assert.equal(body, 'File not found\n');
+      done();
+    });
+  });
+
+  it('sends package.json', function(done) {
+    request({
+      url: `http://localhost:${PORT}/package.json`,
+      json: true
+    }, (error, res, body) => {
+      if(error) {
+        return done(error);
+      }
+
+      assert(body.description, 'A picture sharing application!');
+      done();
+    });
   });
 });
